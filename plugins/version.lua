5b1fa285e8e39e0dd0787a5fa9547a248e571dce
mrvigeo <maryamhojaty3@gmail.com>
2016-02-01 05:06:43 -0800

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,16 @@
+do
+
+function run(msg, matches)
+  return ' Bot v1.5'.. [[ 
+  
+Powered by @Mr_Vigeo tnx to SBSS team
+end
+return {
+  description = "Robot and Creator About", 
+  usage = "ver : robot info",
+  patterns = {
+    "^ver$"
+  }, 
+  run = run 
+}
+end
